6cc15c6eeda7bca2b2e9698c75d9c022ffc776a6
Lisa <fanciful.mischief@gmail.com>
2020-05-13 11:17:57 +0300

    fixed ddate and rdate

--- reservations.py
+++ reservations.py
@@ -10,8 +10,8 @@ from search import get_flight_info, get_website_response, print_flight_info
 def find_flights():
     """a method that is providing information about flights"""
     iata_from, iata_to, flights_type, departure_date, return_date = get_user_options()
-    ddate = str(datetime.strptime(departure_date, "%Y-%m-%d").date().strftime("%Y_%m_%d"))
-    rdate = str(datetime.strptime(return_date, "%Y-%m-%d").date().strftime("%Y_%m_%d"))
+    ddate = datetime.strptime(departure_date, "%Y-%m-%d").strftime("%Y_%m_%d")
+    rdate = datetime.strptime(return_date, "%Y-%m-%d").strftime("%Y_%m_%d")
     print('\nsearch results: ')
     if flights_type == 1:  # corresponds to the round trip
         body = html.fromstring(get_website_response(iata_from, iata_to, departure_date,
